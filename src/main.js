171a3b4f5e3c39804464475363c18a07ba079cc6
Mikey Kriegel <mikkrieg@gmail.com>
2021-02-05 13:44:19 -0800

    Removed option values in html, finish refactoring UI logic

--- src/main.js
+++ src/main.js
@@ -12,12 +12,11 @@ $(document).ready(function(){
     const userPlanet = $('select').val();
     let user = new User(userAge, userLifeE);
 
-    if(userPlanet === 'Mercury'|| 'Venus' || 'Mars' || 'Jupiter'){
-      user.mercury();
-      console.log(user);
-      if(user.age < user.lifeE){
+    if(userPlanet === 'Mercury'|| userPlanet === 'Venus' || userPlanet === 'Mars' || userPlanet === 'Jupiter'){
+      user[userPlanet.toLowerCase()]();
+      if(user.age <= user.lifeE){
         user.yearsLeft()
-        $('div#output').text(`You picked: ${userPlanet}, on ${userPlanet} you would be ${user.age} ${userPlanet} years old and your life expectancy would be ${user.lifeE} ${userPlanet} years! You would have ${user.yearsLeft} to live on ${userPlanet}!`);
+        $('div#output').text(`You picked: ${userPlanet}, on ${userPlanet} you would be ${user.age} ${userPlanet} years old and your life expectancy would be ${user.lifeE} ${userPlanet} years! You would have ${user.yearsLeft} years left to live on ${userPlanet}!`);
       } else {
         user.yearsDead();
         $('div#output').text(`You picked: ${userPlanet}, on ${userPlanet} you would be ${user.age} ${userPlanet} years old and your life expectancy would be ${user.lifeE} ${userPlanet} years! You would be ${user.yearsDead} years older than your life expectancy on ${userPlanet}!`);
