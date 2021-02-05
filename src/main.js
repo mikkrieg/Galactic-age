be1a4bdde52fef64a2e5faae694ad02bd4c74958
Mikey Kriegel <mikkrieg@gmail.com>
2021-02-05 15:09:54 -0800

    add more styling, add minimum input, change wording of result

--- src/main.js
+++ src/main.js
@@ -17,11 +17,17 @@ $(document).ready(function(){
       $('div#output-card').show();
       if(user.age <= user.lifeE){
         user.yearsLeft()
-        $('div#output').text(`You picked ${userPlanet}! On ${userPlanet} you would be ${user.age} ${userPlanet} years old and your life expectancy would be ${user.lifeE} ${userPlanet} years! You would have ${user.yearsLeft} years left to live on ${userPlanet}!`);
+        $('div#output').text(`You picked ${userPlanet}! There you would be ${user.age} ${userPlanet} years old and your life expectancy would be ${user.lifeE} ${userPlanet} years! You would have ${user.yearsLeft} years left to live on ${userPlanet}!`);
       } else {
         user.yearsDead();
-        $('div#output').text(`You picked: ${userPlanet}, on ${userPlanet} you would be ${user.age} ${userPlanet} years old and your life expectancy would be ${user.lifeE} ${userPlanet} years! You would be ${user.yearsDead} years older than your life expectancy on ${userPlanet}!`);
+        $('div#output').text(`You picked ${userPlanet}! There you would be ${user.age} ${userPlanet} years old and your life expectancy would be ${user.lifeE} ${userPlanet} years! You would be ${user.yearsDead} years older than your life expectancy on ${userPlanet}!`);
       }
     }
+    // Scrolls down to result on smaller screens
+    window.scroll({
+      top: 150,
+      left: 50,
+      behavior: 'smooth'
+    });
   });
 });
