55ca4cad9f2bf6872e34fd4c74f82fce18eb17c8
Mikey Kriegel <mikkrieg@gmail.com>
2021-02-05 11:29:09 -0800

    Fix imports in main.js, start creating UI in index.html

--- src/main.js
+++ src/main.js
@@ -1,9 +1,9 @@
-import {User} from "./js/age.js";
-import $ from 'jqeury';
+// import {User} from "./js/age.js";
+import $ from 'jquery';
 import 'bootstrap';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './css/styles.css';
 
 $(document).ready(function(){
-  
+
 });
