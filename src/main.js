d3083f558b10b09179c3d51b9cf02a28f97e2536
Mikey Kriegel <mikkrieg@gmail.com>
2021-02-05 13:31:08 -0800

    Add values to html options, add beginning of UI logic

--- src/main.js
+++ src/main.js
@@ -1,5 +1,5 @@
-// import {User} from "./js/age.js";
 import $ from 'jquery';
+import {User} from "./js/age.js";
 import 'bootstrap';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './css/styles.css';
@@ -9,7 +9,19 @@ $(document).ready(function(){
     e.preventDefault();
     const userAge = $('input#age').val();
     const userLifeE = $('input#life-exp').val();
-    userAge;
-    userLifeE;
+    const userPlanet = $('select').val();
+    let user = new User(userAge, userLifeE);
+
+    if(userPlanet === 'Mercury'|| 'Venus' || 'Mars' || 'Jupiter'){
+      user.mercury();
+      console.log(user);
+      if(user.age < user.lifeE){
+        user.yearsLeft()
+        $('div#output').text(`You picked: ${userPlanet}, on ${userPlanet} you would be ${user.age} ${userPlanet} years old and your life expectancy would be ${user.lifeE} ${userPlanet} years! You would have ${user.yearsLeft} to live on ${userPlanet}!`);
+      } else {
+        user.yearsDead();
+        $('div#output').text(`You picked: ${userPlanet}, on ${userPlanet} you would be ${user.age} ${userPlanet} years old and your life expectancy would be ${user.lifeE} ${userPlanet} years! You would be ${user.yearsDead} years older than your life expectancy on ${userPlanet}!`);
+      }
+    }
   });
 });
