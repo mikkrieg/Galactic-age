093871a7fdc2567f64ce74f0095ec357d5e0cc64
Mikey Kriegel <mikkrieg@gmail.com>
2021-02-05 14:24:44 -0800

    Change wording in main.js, add rows to html, add more styling

--- src/main.js
+++ src/main.js
@@ -17,7 +17,7 @@ $(document).ready(function(){
       $('div#output-card').show();
       if(user.age <= user.lifeE){
         user.yearsLeft()
-        $('div#output').text(`You picked: ${userPlanet}, on ${userPlanet} you would be ${user.age} ${userPlanet} years old and your life expectancy would be ${user.lifeE} ${userPlanet} years! You would have ${user.yearsLeft} years left to live on ${userPlanet}!`);
+        $('div#output').text(`You picked ${userPlanet}! On ${userPlanet} you would be ${user.age} ${userPlanet} years old and your life expectancy would be ${user.lifeE} ${userPlanet} years! You would have ${user.yearsLeft} years left to live on ${userPlanet}!`);
       } else {
         user.yearsDead();
         $('div#output').text(`You picked: ${userPlanet}, on ${userPlanet} you would be ${user.age} ${userPlanet} years old and your life expectancy would be ${user.lifeE} ${userPlanet} years! You would be ${user.yearsDead} years older than your life expectancy on ${userPlanet}!`);
