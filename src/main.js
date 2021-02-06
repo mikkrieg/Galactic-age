bd7afb44d1642a288a4416693480954e0d918fad
Mikey Kriegel <mikkrieg@gmail.com>
2021-02-05 16:41:50 -0800

    remove birthday input, make grammer edits to template literal

--- src/main.js
+++ src/main.js
@@ -9,19 +9,19 @@ $(document).ready(function(){
     e.preventDefault();
     const userAge = $('input#age').val();
     const userLifeE = $('input#life-exp').val();
-    const userBirthday = $('input#birthday').val();
     const userPlanet = $('select').val();
-    let user = new User(userAge, userLifeE, userBirthday);
+    let user = new User(userAge, userLifeE);
 
     if(userPlanet === 'Mercury'|| userPlanet === 'Venus' || userPlanet === 'Mars' || userPlanet === 'Jupiter'){
       user[userPlanet.toLowerCase()]();
+      user.mayFly();
       $('div#output-card').show();
       if(user.age <= user.lifeE){
         user.yearsLeft()
-        $('div#output').text(`You picked ${userPlanet}! There you would be ${user.age} ${userPlanet} years old and your life expectancy would be ${user.lifeE} ${userPlanet} years! You would have ${user.yearsLeft} years left to live on ${userPlanet}!`);
+        $('div#output1').text(`You picked ${userPlanet}! There you would be ${user.age} ${userPlanet} years old and your life expectancy would be ${user.lifeE} ${userPlanet} years! You would have ${user.yearsLeft} years left to live on ${userPlanet}! If you were a mayfly on earth you would have lived ${user.mayFlyLives} lifetimes, wow! In relation to the suns age in earth years, that would be only ${user.sunComparison}% of its age`);
       } else {
         user.yearsDead();
-        $('div#output').text(`You picked ${userPlanet}! There you would be ${user.age} ${userPlanet} years old and your life expectancy would be ${user.lifeE} ${userPlanet} years! You would be ${user.yearsDead} years older than your life expectancy on ${userPlanet}!`);
+        $('div#output1').text(`You picked ${userPlanet}! There you would be ${user.age} ${userPlanet} years old and your life expectancy would be ${user.lifeE} ${userPlanet} years! You would be ${user.yearsDead} years older than your life expectancy on ${userPlanet}! If you were a mayfly on earth, you would have lived ${user.mayFlyLives} mayfly lifetimes, wow! If you turned those lifetimes into earth years and compared them to the age of our sun in earth years, it would only be ${user.sunComparison}% of its age`);
       }
     }
     // Scrolls down to result on smaller screens
