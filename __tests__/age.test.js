adee603d280d0063a82fe5f014136ab8b57999ca
Mikey Kriegel <mikkrieg@gmail.com>
2021-02-11 18:44:47 -0800

    Create two tests for each part of deadOrAlive function, both pass

--- __tests__/age.test.js
+++ __tests__/age.test.js
@@ -52,4 +52,19 @@ describe('User', () => {
     expect(reusableUser.mayFlyLives).toEqual(2628000);
     expect(reusableUser.sunComparison).toEqual('0.058');
   });
+
+  test('should calculate if a user is not past their life expectancy', () => {
+    const userPlanet = 'Mercury' || 'Venus' || 'Mars' || 'Jupiter';
+    reusableUser.deadOrAlive(userPlanet);
+    expect(reusableUser.message1).toMatch(`You picked ${userPlanet}! There you would be ${reusableUser.age} ${userPlanet} years old and your life expectancy would be ${reusableUser.lifeE} ${userPlanet} years! You would have ${reusableUser.yearsLeft} years left to live on ${userPlanet}!`);
+    expect(reusableUser.message2).toMatch(`If you were a mayfly on earth, you would have lived ${reusableUser.mayFlyLives} lifetimes, wow! In relation to the suns age in earth years, that would be only ${reusableUser.sunComparison}% of its age`);
+  });
+
+  test('should calculate if a user is past their life expectancy', () => {
+    const userPlanet = 'Venus' || 'Mars' || 'Jupiter';
+    const oldUser = new User(90, 70);
+    oldUser.deadOrAlive(userPlanet);
+    expect(oldUser.message1).toMatch(`You picked ${userPlanet}! There you would be ${oldUser.age} ${userPlanet} years old and your life expectancy would be ${oldUser.lifeE} ${userPlanet} years! You would be ${oldUser.yearsDead} years older than your life expectancy on ${userPlanet}!`);
+    expect(oldUser.message2).toMatch(`If you were a mayfly on earth, you would have lived ${oldUser.mayFlyLives} lifetimes, wow! In relation to the suns age in earth years, that would be only ${oldUser.sunComparison}% of its age`);
+  });
 });
